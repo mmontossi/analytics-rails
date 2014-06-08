812f29fede3a4c9a3f967fae439f169186e2442c
Mathías Montossi <mmontossi@museways.com>
2014-06-08 18:49:52 -0300

    Fix rails 3 compatibility

--- test/dummy/config/initializers/secret_token.rb
+++ test/dummy/config/initializers/secret_token.rb
@@ -9,4 +9,5 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
+Dummy::Application.config.secret_token = 'cc799f48bef8d600576f83d4fbee274f1d876f156f6b3a18116abe76c81b0153e82bc9bdcf898aca59e1532e5d1a29c7bbcf592d0d9ad15ccecec650fa5ff5e7'
 Dummy::Application.config.secret_key_base = 'cc799f48bef8d600576f83d4fbee274f1d876f156f6b3a18116abe76c81b0153e82bc9bdcf898aca59e1532e5d1a29c7bbcf592d0d9ad15ccecec650fa5ff5e7'
