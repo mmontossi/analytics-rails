3a683c591ba9ff83b36388bca51149c7bf0797b9
Mathías Montossi <mmontossi@museways.com>
2015-07-04 15:16:33 -0300

    Improve test readability

--- test/include_tag_test.rb
+++ test/include_tag_test.rb
@@ -5,9 +5,11 @@ class IncludeTagTest < ActionView::TestCase
   test 'tag id' do
     with_env 'production' do
       assert google_analytics_include_tag('id').include?("_gaq.push(['_setAccount', 'id']);")
+
       assert google_analytics_include_tag(
         variables: [[1, 'Member', 'Yes', 1]]
       ).include?("_gaq.push(['_setCustomVar', 1, 'Member', 'Yes', 1])")
+
       assert google_analytics_include_tag(
         events: [['Popup', 'Click']]
       ).include?("ga('send', 'event', 'Popup', 'Click');")
