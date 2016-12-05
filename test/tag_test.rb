bfc74c6e0e2f929fbd7dd08b72bbc3eb97a035fd
Mathías Montossi <mmontossi@buyin.io>
2016-12-05 18:16:00 -0300

    Minor improvement

--- test/tag_test.rb
+++ test/tag_test.rb
@@ -3,14 +3,17 @@ require 'test_helper'
 class TagTest < ActionView::TestCase
 
   test 'include' do
+    self.request = ActionController::TestRequest.new
     with_env 'production' do
       tag = google_analytics_include_tag('id')
       assert_includes tag, 'ga("create", "id", "auto");'
       assert_includes tag, 'ga("send", "pageview", {});'
+      assert_includes tag, 'http://www.google-analytics.com/analytics.js'
 
       tag = google_analytics_include_tag('id', other: 'value')
       assert_includes tag, 'ga("create", "id", "auto");'
       assert_includes tag, 'ga("send", "pageview", {"other":"value"});'
+      assert_includes tag, 'http://www.google-analytics.com/analytics.js'
     end
 
     with_env 'development' do
@@ -24,6 +27,7 @@ class TagTest < ActionView::TestCase
         google_analytics_event_tag('Popup', 'click'),
         'ga("send", "event", "Popup", "click", {});'
       )
+
       assert_includes(
         google_analytics_event_tag('Video', 'play', 'ad.mp4', 10, non_interactive: 'value'),
         'ga("send", "event", "Video", "play", "ad.mp4", 10, {"nonInteractive":"value"});'
